42121f9bbff040e79dc96468c429aad807c4bdad
Kurt Hakim <kurthakim@gmail.com>
2023-03-31 07:51:46 +0700

    Finishing setting up our application, still has error

--- internal/handlers/perform-checks.go
+++ internal/handlers/perform-checks.go
@@ -35,32 +35,55 @@ type jsonResp struct {
 func (repo *DBRepo) TestCheck(w http.ResponseWriter, r *http.Request) {
 	hostServiceID, _ := strconv.Atoi(chi.URLParam(r, "id"))
 	oldStatus := chi.URLParam(r, "oldStatus")
-
-	log.Println(hostServiceID, oldStatus)
+	okay := true
 
 	// get host service
 	hs, err := repo.DB.GetHostServiceByID(hostServiceID)
 	if err != nil {
 		log.Println(err)
-		return
+		okay = false
 	}
 
-	log.Println("Service name is", hs.Service.ServiceName)
 	// get host
 	h, err := repo.DB.GetHostByID(hs.HostID)
 	if err != nil {
 		log.Println(err)
-		return
+		okay = false
 	}
 
 	// test the service
 	newStatus, msg := repo.testServiceForHost(h, hs)
-	log.Println(newStatus, msg)
+
+	// update the host service in the database with status (if changed) and last check
+	hs.Status = newStatus
+	hs.LastCheck = time.Now()
+	hs.UpdatedAt = time.Now()
+
+	err = repo.DB.UpdateHostService(hs)
+	if err != nil {
+		log.Println(err)
+		okay = false
+	}
+
+	// broadcast service status changed event
 
 	// create json
-	resp := jsonResp{
-		OK:      true,
-		Message: "test message",
+	var resp jsonResp
+
+	if okay {
+		resp = jsonResp{
+			OK:            true,
+			Message:       msg,
+			ServiceID:     hs.ServiceID,
+			HostServiceID: hs.ID,
+			HostID:        hs.HostID,
+			OldStatus:     oldStatus,
+			NewStatus:     newStatus,
+			LastCheck:     time.Now(),
+		}
+	} else {
+		resp.OK = false
+		resp.Message = "Something went wrong"
 	}
 
 	// send json to client
